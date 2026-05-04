3e638edd32566fbb00f50bbdd0257b2b70cbb83f
Akho10 <APhungula@sars.gov.za>
2026-05-04 13:01:22 +0200

    Added add operation

--- ArithmeticCalculator/src/package1/Addition.java
+++ ArithmeticCalculator/src/package1/Addition.java
@@ -2,4 +2,9 @@ package package1;
 
 public class Addition {
 
+	
+	
+	public double add(double num1, double num2) {
+		return num1 + num2;
+	}
 }
--- ArithmeticCalculator/src/package1/Main.java
+++ ArithmeticCalculator/src/package1/Main.java
@@ -6,8 +6,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Addition sum = new Addition();
+		
+		double number1 = 11;
+		double number2 = 3;
 		
 		
+		//menu
+		int choice = 1;
+		double result = 0;
+		
+		switch(choice) {
+		case 1:
+			result = sum.add(number1, number2);
+			break;
+			
+		case 2:
+			break;
+			
+		case 3:
+			break;
+			
+		case 4:
+			break;
+		}
+		
+		System.out.println("Result: " + result);
 
 	}
 
