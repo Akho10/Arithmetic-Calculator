49949806a1d44a5bddc729dc8a463cf8ad42bff4
Akho10 <APhungula@sars.gov.za>
2026-05-04 13:11:15 +0200

    Added scanner and subtraction operation

--- ArithmeticCalculator/src/package1/Main.java
+++ ArithmeticCalculator/src/package1/Main.java
@@ -1,27 +1,35 @@
 package package1;
-
+import java.util.*;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
 		Addition sum = new Addition();
+		Subtraction sub = new Subtraction();
 		
 		double number1 = 11;
 		double number2 = 3;
 		
-		
-		//menu
-		int choice = 1;
+		System.out.println("Choose maths operation: ");
+		System.out.println("1. Addition");
+		System.out.println("2.Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+        int choice = scanner.nextInt();
 		double result = 0;
 		
 		switch(choice) {
 		case 1:
 			result = sum.add(number1, number2);
+			System.out.println("Addition result: " + result);
 			break;
 			
 		case 2:
+			result = sub.subtract(number1, number2);
+			System.out.println("Subtratcion result: " + result);
 			break;
 			
 		case 3:
@@ -31,7 +39,8 @@ public class Main {
 			break;
 		}
 		
-		System.out.println("Result: " + result);
+		
+		
 
 	}
 
--- ArithmeticCalculator/src/package1/Subtraction.java
+++ ArithmeticCalculator/src/package1/Subtraction.java
@@ -2,4 +2,7 @@ package package1;
 
 public class Subtraction {
 
+	public double subtract(double num1, double num2) {
+		return num1 - num2;
+	}
 }
